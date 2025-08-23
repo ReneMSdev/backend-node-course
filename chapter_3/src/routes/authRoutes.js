bdbc086dbc0e5a860f533535d1fe556860132c9a
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-08-23 16:22:48 +0700

    logic for login route

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -44,6 +44,28 @@ router.post('/login', (req, res) => {
   // we get their email, and we look up the password associated with that email in the db
   // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
   // so we can again one way encrypt the password the user just entered
+
+  const { username, password } = req.body
+
+  try {
+    const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+    const user = getUser.get(username)
+
+    // if we can't find a user associated with that username, return out of the function
+    if (!user) return res.status(404).send({ message: 'User not found' })
+
+    // Synchronously tests a password against a hash.
+    const passwordIsValid = bcrypt.compareSync(password, user.password)
+    // If the password does not match return out of the function
+    if (!passwordIsValid) return res.status(401).send({ message: 'Invalid password' })
+
+    console.log(user)
+    // then we have a successful authentication
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+  } catch (err) {
+    console.log(err.message)
+    res.sendStatus(503)
+  }
 })
 
 export default router
